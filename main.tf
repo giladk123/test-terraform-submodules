40f3604c610c285402f1e7b83cfe91632d3cdd84
giladk123 <giladk@gmail.com>
2024-07-06 18:02:04 +0300

    test

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ locals {
 
 module "spoke" {
   source  = "app.terraform.io/hcta-azure-dev/spoke/azurerm"
-  version = "1.0.2"
+  version = "1.0.3"
  
   resource_groups = local.resource_group.resource_groups
   
