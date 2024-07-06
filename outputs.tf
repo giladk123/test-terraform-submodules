16531f0e007496fa46a26bd34f68a0fcf2c4d254
giladk123 <giladk@gmail.com>
2024-07-06 22:01:50 +0300

    added outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "resource-groups" {
   value = module.spoke.resource-groups
   description = "value of resource-groups"
 }
+
+output "vnets" {
+  value = module.spoke.vnets
+  description = "value of vnets"
+}
