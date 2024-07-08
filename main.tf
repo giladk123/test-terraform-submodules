b58901db53f409dbd4c813bd09260a321a01e5de
giladk123 <giladk@gmail.com>
2024-07-08 22:26:56 +0300

    added submodule keyvault

--- main.tf
+++ main.tf
@@ -2,14 +2,16 @@
 locals {
   resource_group          = jsondecode(file("./ccoe/rg.json"))
   vnet_settings           = jsondecode(file("./network/vnet.json"))
+  keyvault_settings       = jsondecode(file("./ccoe/keyvault.json"))
 }
 
 module "spoke" {
   source  = "app.terraform.io/hcta-azure-dev/spoke/azurerm"
-  version = "1.0.11"
+  version = "1.0.12"
  
   resource_groups = local.resource_group.resource_groups
   vnets = local.vnet_settings.vnets
+  keyvaults = local.keyvault_settings.keyvaults
   providers = {
     azurerm = azurerm.subscription1
   }
--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "vnets" {
   value = module.spoke.vnets
   description = "value of vnets"
 }
+
+output "keyvaults" {
+  value = module.spoke.keyvaults
+  description = "value of keyvaults"
+}
