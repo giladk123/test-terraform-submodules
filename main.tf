f7915b35147a637c5fa45bb382032712fb8f3d99
giladk123 <giladk@gmail.com>
2024-07-13 10:22:36 +0300

    added private endpoint

--- main.tf
+++ main.tf
@@ -20,3 +20,26 @@ module "spoke" {
   }
 }
 
+module "spoke_private-endpoint" {
+  source  = "app.terraform.io/hcta-azure-dev/spoke/azurerm//modules/private-endpoint"
+  version = "1.0.18"
+  # insert required variables here
+  
+  endpoints = {
+    "keyvault-endpoint": {
+      "name": "we-ydev-azus-opdx-01-kv01-pe",
+      "resource_group_name": module.spoke.resource-groups["we-ydev-azus-opdx-arutzim-rg"].name,
+      "subnet_id": module.spoke.subnet["we-ydev-azus-opdx-crm-vnet-keyvault"].id,
+      "private_dns_zone_id": module.spoke.dns_zones["privatelink.vaultcore.azure.net"],
+      "location": "westeurope",
+      "private_connection_resource_id": module.spoke.keyvaults["we-ydev-azus-opdx-01-kv"].id,
+      "subresource_names": ["vault"]
+    }
+  }
+
+  depends_on = [ module.spoke ]
+
+  providers = {
+    azurerm = azurerm.subscription1
+  }
+}
