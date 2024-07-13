d4e8bd9b2e883710ad3febc6417b7e0a90321476
giladk123 <giladk@gmail.com>
2024-07-13 15:05:39 +0300

    test module fondation

--- main.tf
+++ main.tf
@@ -6,40 +6,40 @@ locals {
   dns_zones         = jsondecode(file("./ccoe/private-dns-zones.json"))
 }
 
-module "spoke" {
-  source  = "app.terraform.io/hcta-azure-dev/spoke/azurerm"
-  version = "1.0.20"
-
+module "foundation" {
+  source  = "app.terraform.io/hcta-azure-dev/foundation/azurerm"
+  version = "1.0.3"
+  
   resource_groups     = local.resource_group.resource_groups
   vnets               = local.vnet_settings.vnets
-  keyvaults           = local.keyvault_settings.keyvaults
-  zones               = local.dns_zones.zones
-  resource_group_name = local.dns_zones.resource_group_name
-  providers = {
-    azurerm = azurerm.subscription1
-  }
-}
-
-module "spoke_private-endpoint" {
-  source  = "app.terraform.io/hcta-azure-dev/spoke/azurerm//modules/private-endpoint"
-  version = "1.0.20"
-  # insert required variables here
   
-  endpoints = {
-    "keyvault-endpoint": {
-      "name": "we-ydev-azus-opdx-01-kv01-pe",
-      "resource_group_name": module.spoke.resource-groups["we-ydev-azus-opdx-arutzim-rg"].name,
-      "subnet_id": module.spoke.subnets["we-ydev-azus-opdx-crm-vnet-keyvault"].id,
-      "private_dns_zone_id": module.spoke.dns_zones["privatelink.vaultcore.azure.net"],
-      "location": "westeurope",
-      "private_connection_resource_id": module.spoke.keyvaults["we-ydev-azus-opdx-01-kv"].id,
-      "subresource_names": ["vault"]
-    }
-  }
-
-  depends_on = [ module.spoke ]
-
   providers = {
     azurerm = azurerm.subscription1
   }
 }
+
+
+
+# module "spoke_private-endpoint" {
+#   source  = "app.terraform.io/hcta-azure-dev/spoke/azurerm//modules/private-endpoint"
+#   version = "1.0.20"
+#   # insert required variables here
+  
+#   endpoints = {
+#     "keyvault-endpoint": {
+#       "name": "we-ydev-azus-opdx-01-kv01-pe",
+#       "resource_group_name": module.spoke.resource-groups["we-ydev-azus-opdx-arutzim-rg"].name,
+#       "subnet_id": module.spoke.subnets["we-ydev-azus-opdx-crm-vnet-keyvault"].id,
+#       "private_dns_zone_id": module.spoke.dns_zones["privatelink.vaultcore.azure.net"],
+#       "location": "westeurope",
+#       "private_connection_resource_id": module.spoke.keyvaults["we-ydev-azus-opdx-01-kv"].id,
+#       "subresource_names": ["vault"]
+#     }
+#   }
+
+#   depends_on = [ module.spoke ]
+
+#   providers = {
+#     azurerm = azurerm.subscription1
+#   }
+# }
--- outputs.tf
+++ outputs.tf
@@ -1,30 +1,29 @@
 output "resource-groups" {
-  value       = module.spoke.resource-groups
+  value       = module.foundation.resource-groups
   description = "value of resource-groups"
 }
 
 output "vnets" {
-  value       = module.spoke.vnets
+  value       = module.foundation.vnets
   description = "value of vnets"
 }
 
 output "subnets" {
-  value       = module.spoke.subnets
-  description = "value of subnets"
-  
+  value       = module.foundation.subnets
+  description = "value of subnets"  
 }
 
-output "keyvaults" {
-  value       = module.spoke.keyvaults
-  description = "value of keyvaults"
-}
+# output "keyvaults" {
+#   value       = module.spoke.keyvaults
+#   description = "value of keyvaults"
+# }
 
-output "dns_zones" {
-  value       = module.spoke.dns_zones
-  description = "value of dns_zones"
-}
+# output "dns_zones" {
+#   value       = module.spoke.dns_zones
+#   description = "value of dns_zones"
+# }
 
-output "private-endpoint" {
-  value       = module.spoke_private-endpoint.private_endpoints
-  description = "value of private-endpoint"
-}
+# output "private-endpoint" {
+#   value       = module.spoke_private-endpoint.private_endpoints
+#   description = "value of private-endpoint"
+# }
